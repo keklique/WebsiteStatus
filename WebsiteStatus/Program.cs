876107fd040ae89ed044987364c684ce3d96979f
keklique <41877582+keklique@users.noreply.github.com>
2023-01-15 22:53:40 +0300

    Basic webstatus logger created

--- WebsiteStatus/Program.cs
+++ WebsiteStatus/Program.cs
@@ -1,10 +1,37 @@
+using Serilog;
+using Microsoft.Extensions.Hosting;
 using WebsiteStatus;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService()
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
     })
+    .UseSerilog()
     .Build();
 
-await host.RunAsync();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.File(@"C:\temp\workerservice\LogFile.txt")
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting up the service");
+    await host.RunAsync();
+    return;
+}
+catch(Exception ex)
+{
+    Log.Fatal(ex, "There was a problem starting the service");
+    return;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+
--- WebsiteStatus/Worker.cs
+++ WebsiteStatus/Worker.cs
@@ -3,10 +3,22 @@ namespace WebsiteStatus
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-
+        private HttpClient client;
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.Dispose();
+            return base.StopAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -14,7 +26,16 @@ namespace WebsiteStatus
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                var result = await client.GetAsync("https://api.seriousenglish.app/wordlist");
+                if(result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("The website is up. Status code {StatusCode}", result.StatusCode);
+                }
+                else
+                {
+                    _logger.LogError("The website is down.  Status code {StatusCode}", result.StatusCode);
+                }
+                await Task.Delay(5*60*1000, stoppingToken);
             }
         }
     }
